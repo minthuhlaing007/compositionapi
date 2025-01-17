775394ab560cd4605c75c958cb3f19886cebd7b0
minthuhlaing007 <minthuhlaing786@gmail.com>
2025-01-17 22:33:16 +0630

    computed property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,60 +1,35 @@
 <template>
   <div class="home">
-    <h1>Person One</h1>
-    <h3>{{ personOne.name }} -- {{ personOne.age }}</h3>
-    <button @click="changeOne">Change one</button>
-    <h1>Person Two</h1>
-    <h3>{{ personTwo.name }} -- {{ personTwo.age }}</h3>
-    <button @click="changeTwo">Change two</button>
-    <br />
-    <br />
-    <h1>User One</h1>
-    <h3>{{ userOne }}</h3>
-    <button @click="changeUserOne">change user one</button>
-    <h1>User Two</h1>
-    <h3>{{ userTwo }}</h3>
-    <button @click="changeUserTwo">change user two</button>
+    <input type="text" v-model="search" />
+    <h2>Search : {{ search }}</h2>
+    <div v-for="name in filteredName" :key="name">
+      <h2>{{ name }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, ref } from "vue";
+
 export default {
   setup() {
-    let personOne = ref({ name: "kyaw kyaw", age: 22 });
-    let changeOne = () => {
-      personOne.value.name = "john doe";
-    };
+    let names = ref([
+      "aung aung",
+      "kyaw kyaw",
+      "maung maung",
+      "su su",
+      "john doe",
+      "alice",
+      "bob",
+    ]);
+    let search = ref("");
+    let filteredName = computed(() => {
+      return names.value.filter((name) => {
+        return name.includes(search.value);
+      });
+    });
 
-    // ref ko tone mal so yin value ko target lote mha tar mi late mal reactive ko tone mal so yin top tan khaw loa ya tal for example( ref = user.value.name, reactive = user.name)
-    let personTwo = reactive({ name: "hlaing min than", age: 11 });
-    let changeTwo = () => {
-      personTwo.name = "aung aung";
-    };
-
-    // ---------------------------------------------------------------------------------------------------
-
-    let userOne = ref("hlaingminthan");
-    let changeUserOne = () => {
-      userOne.value = "change hmt";
-    };
-
-    // reactive so tat function ka object tway array tway mar bae a lote lote par tal ; primitive value ko reactive lote loa ma ya buu ; primitive(String,number,boolean);
-    let userTwo = reactive("aung aung");
-    let changeUserTwo = () => {
-      userTwo = "change aa";
-    };
-
-    return {
-      personOne,
-      changeOne,
-      personTwo,
-      changeTwo,
-      userOne,
-      userTwo,
-      changeUserOne,
-      changeUserTwo,
-    };
+    return { names, search, filteredName };
   },
 };
 </script>
