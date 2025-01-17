50335616b067b34a108042f010c00d55a63472c7
minthuhlaing <minthuhlaing786@gmail.com>
2025-01-17 20:59:09 +0630

    compare ref and reactive function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,60 @@
 <template>
   <div class="home">
-    <h1>Composition api</h1>
-    <h2>My name is {{name}}</h2>
-    <p>I am {{age}} years old.</p>
-    <button @click="clickHandler">Click</button>
-    <br> <br>
-    <input type="text" v-model="name">
-    <br> <br>
-    <button @click='age++'>Increase Age</button>
+    <h1>Person One</h1>
+    <h3>{{ personOne.name }} -- {{ personOne.age }}</h3>
+    <button @click="changeOne">Change one</button>
+    <h1>Person Two</h1>
+    <h3>{{ personTwo.name }} -- {{ personTwo.age }}</h3>
+    <button @click="changeTwo">Change two</button>
+    <br />
+    <br />
+    <h1>User One</h1>
+    <h3>{{ userOne }}</h3>
+    <button @click="changeUserOne">change user one</button>
+    <h1>User Two</h1>
+    <h3>{{ userTwo }}</h3>
+    <button @click="changeUserTwo">change user two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
+import { reactive, ref } from "vue";
 export default {
-  setup(){
-    let name = ref("john doe"); // reactive value
-    // console.log(name);
-    let age = ref(20);
-    let clickHandler=()=>{
-      // reactive value ko khaw tone mal so yin value htae pay ka tal 
-      name.value = "kyaw kyaw";
-    }
+  setup() {
+    let personOne = ref({ name: "kyaw kyaw", age: 22 });
+    let changeOne = () => {
+      personOne.value.name = "john doe";
+    };
 
-    // data twae ko khaw tone chin yin return pyan pay ka mal
-    return{name,age,clickHandler};
-  }
-}
+    // ref ko tone mal so yin value ko target lote mha tar mi late mal reactive ko tone mal so yin top tan khaw loa ya tal for example( ref = user.value.name, reactive = user.name)
+    let personTwo = reactive({ name: "hlaing min than", age: 11 });
+    let changeTwo = () => {
+      personTwo.name = "aung aung";
+    };
+
+    // ---------------------------------------------------------------------------------------------------
+
+    let userOne = ref("hlaingminthan");
+    let changeUserOne = () => {
+      userOne.value = "change hmt";
+    };
+
+    // reactive so tat function ka object tway array tway mar bae a lote lote par tal ; primitive value ko reactive lote loa ma ya buu ; primitive(String,number,boolean);
+    let userTwo = reactive("aung aung");
+    let changeUserTwo = () => {
+      userTwo = "change aa";
+    };
+
+    return {
+      personOne,
+      changeOne,
+      personTwo,
+      changeTwo,
+      userOne,
+      userTwo,
+      changeUserOne,
+      changeUserTwo,
+    };
+  },
+};
 </script>
