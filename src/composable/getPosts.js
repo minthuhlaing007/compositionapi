f4fbd560c9fa0efb6fb418fd87f9a9162089aba5
minthuhlaing007 <minthuhlaing786@gmail.com>
2025-02-05 12:27:14 +0630

    timestamp feature

--- src/composable/getPosts.js
+++ src/composable/getPosts.js
@@ -1,21 +1,24 @@
 import { ref } from "vue";
-import {db} from "../firebase/config"
-let getPosts=()=>{
-    let posts=ref([]);
-      let error=ref("");
-      let load=async()=>{
-         try{
-           let res=await db.collection("posts").get()
-               posts.value=res.docs.map((doc)=>{
-                  return {id:doc.id,...doc.data()}
-                  // console.log(doc.id);
-               })
-               // console.log(res.docs)               
-         }catch(err){
-            // console.log(error.message)
-            error.value=err.message;
-         }
-      }
-      return {posts,error,load};
-}
+import { db } from "../firebase/config";
+let getPosts = () => {
+  let posts = ref([]);
+  let error = ref("");
+  let load = async () => {
+    try {
+      let res = await db
+        .collection("posts")
+        .orderBy("created_at", "desc")
+        .get();
+      posts.value = res.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+        // console.log(doc.id);
+      });
+      // console.log(res.docs)
+    } catch (err) {
+      // console.log(error.message)
+      error.value = err.message;
+    }
+  };
+  return { posts, error, load };
+};
 export default getPosts;
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -17,7 +17,7 @@
 <script>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { db } from "@/firebase/config";
+import { db, timestamp } from "@/firebase/config";
 export default {
   setup() {
     let router = useRouter(); // this.$router nae choot soot sin tuu mhu shi par tal;
@@ -39,6 +39,7 @@ export default {
         title: title.value,
         body: body.value,
         tags: tags.value,
+        created_at: timestamp(),
       };
       let res = await db.collection("posts").add(newPost);
       // redirect user to home page
